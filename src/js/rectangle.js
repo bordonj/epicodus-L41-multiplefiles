d63ba58bdbb90be7e17836ff85b4ec48dc075f4a
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-23 09:57:22 -0700

    add constructor logic, pass first test

--- src/js/rectangle.js
+++ src/js/rectangle.js
@@ -1,4 +1,9 @@
 export default class Rectangle {
-  constructor() {
+  constructor(side1, side2) {
+    this.side1 = side1;
+    this.side2 = side2;
   }
 }
+
+// Good Fail: Add first test and Example Class.
+// Passing: Add constructor logic, pass first test.
